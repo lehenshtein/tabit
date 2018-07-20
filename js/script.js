dffaf2d982daf2a612cdf48bce0ccdedaf318911
Oleksii <ostu@diceus.com>
2018-07-20 19:00:00 +0300

    scroll/scrollbar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//bubble positioning
+
 $("document").ready(function() {
     $(".slider").rangeslider();
 });
@@ -23,3 +25,22 @@ function updateSlider(passObj) {
     nextObj.find("span.bar > span").css("width", percentage + "%");
     nextObj.find("span.bar-btn > span").text(percentage / 4);
 };
+
+//horizontal scrolling
+
+document.getElementById("horizontal-scroller")
+  .addEventListener('wheel', function(event) {
+    if (event.deltaMode == event.DOM_DELTA_PIXEL) {
+      var modifier = 1;
+      // иные режимы возможны в Firefox
+    } else if (event.deltaMode == event.DOM_DELTA_LINE) {
+      var modifier = parseInt(getComputedStyle(this).lineHeight);
+    } else if (event.deltaMode == event.DOM_DELTA_PAGE) {
+      var modifier = this.clientHeight;
+    }
+    if (event.deltaY != 0) {
+      // замена вертикальной прокрутки горизонтальной
+      this.scrollLeft += modifier * event.deltaY;
+      event.preventDefault();
+    }
+  });
